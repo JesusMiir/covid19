e4e8e45f02fa51960a5f35de5d3148ab80d8753e
Jesús Mercadal Mir <j.mercadal@icnea.com>
2020-06-09 19:30:35 +0200

    asdf

--- covid19/agents.py
+++ covid19/agents.py
@@ -42,7 +42,7 @@ class PersonaInfectada(RandomWalker):
 class PersonaMalalta(RandomWalker):
 
     def __init__(self, unique_id, pos, model, moore, temps_malalta):
-        super().__init__(unique_id, pos, model, moore=moore, temps_malalta)
+        super().__init__(unique_id, pos, model, moore=moore)
 
     def step(self):
         self.random_move()
