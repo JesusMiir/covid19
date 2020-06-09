edc3205e810023528d4b691a568b922bbf919b24
Jesús Mercadal Mir <j.mercadal@icnea.com>
2020-06-09 17:30:11 +0200

    a

--- covid19/agents.py
+++ covid19/agents.py
@@ -57,13 +57,13 @@ class Virus(RandomWalker):
 
         if len(persona) > 0:
             persona = self.random.choice(persona)        
-            self.model.grid._remove_agent(self.pos, persona_infectada)
+            self.model.grid._remove_agent(self.pos, persona)
             self.model.schedule.remove(self)
             persona_infectada = PersonaInfectada(
                 self.model.next_id(), self.pos, self.model, self.moore, self.energy
             )
-            self.model.grid.place_agent(lamb, self.pos)
-            self.model.schedule.add(lamb)
+            self.model.grid.place_agent(persona_infectada, self.pos)
+            self.model.schedule.add(persona_infectada)
             
         
 
