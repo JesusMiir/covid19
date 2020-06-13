bd68d839187565421b785b3187f6134471ba445e
Jesús Mercadal Mir <j.mercadal@icnea.com>
2020-06-13 11:40:56 +0200

    a

--- covid19/agents.py
+++ covid19/agents.py
@@ -11,7 +11,7 @@ class PersonaSaludable(RandomWalker):
         self.random_move()
         living = True
 
-        if (self.random.random() * 10) < (self.model.reproduccio_persona):
+        if (self.random.random() * 10) < (self.model.reproduccio_persones):
             virus = PersonaSaludable(
                 self.model.next_id(), self.pos, self.model, self.moore
             )       
