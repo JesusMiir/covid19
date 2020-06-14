d6d848c72a6af995836c0c8985dfc3d7329ac28a
Jesús Mercadal Mir <j.mercadal@icnea.com>
2020-06-14 09:12:28 +0200

    a

--- covid19/agents.py
+++ covid19/agents.py
@@ -63,7 +63,7 @@ class PersonaMalalta(RandomWalker):
     def step(self):
        
         self.durada_malaltia -= 1
-        if self.model.n_random * 10 < self.model.mortalitat_virus or self.durada_malaltia < 0:
+        if self.durada_malaltia < 0 or self.model.n_random * 10 < self.model.mortalitat_virus:
             x, y = self.pos
             this_cell = self.model.grid.get_cell_list_contents([self.pos])
             persona = [obj for obj in this_cell if isinstance(obj, PersonaMalalta)]
