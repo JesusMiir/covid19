8f2061ada5a8d4621b0ca47ffdb788afc2a73d06
Jesús Mercadal Mir <j.mercadal@icnea.com>
2020-06-14 20:21:54 +0200

    a

--- covid19/model.py
+++ covid19/model.py
@@ -26,7 +26,7 @@ class Covid19(Model):
 
     perill_model = 2
     n_random = 0
-    perill = 2
+    perill = 5
 
     verbose = False  # Print-monitoring
 
@@ -53,7 +53,7 @@ class Covid19(Model):
 
         perill_model = 2,
         n_random=0,
-        perill=2
+        perill=5
     ):
 
         super().__init__()
