ffd320812fd22720aed283f7ca800acd4b42a192
Jesús Mercadal Mir <j.mercadal@icnea.com>
2020-06-14 08:51:40 +0200

    a

--- covid19/agents.py
+++ covid19/agents.py
@@ -11,7 +11,7 @@ class PersonaSaludable(RandomWalker):
         self.random_move()
         living = True
 
-        if (self.random.random() * 1000) < (self.model.reproduccio_persones):
+        if (self.model.n_randoom() * 1000) < (self.model.reproduccio_persones):
             virus = PersonaSaludable(
                 self.model.next_id(), self.pos, self.model, self.moore
             )       
@@ -30,12 +30,12 @@ class PersonaInfectada(RandomWalker):
         self.random_move()
         self.temps_deteccio -= 1
         
-        #if (self.random.random() * 10) < (self.model.reproduccio_virus):
-        #    virus = Virus(
-        #        self.model.next_id(), self.pos, self.model, self.moore, self.random.random() * 50
-        #    )       
-        #    self.model.grid.place_agent(virus, self.pos)
-        #    self.model.schedule.add(virus)
+        if (self.model.n_randoom() * 10) < (self.model.reproduccio_virus):
+            virus = Virus(
+                self.model.next_id(), self.pos, self.model, self.moore, self.random.random() * 50
+            )       
+            self.model.grid.place_agent(virus, self.pos)
+            self.model.schedule.add(virus)
         
         if self.temps_deteccio < 0:
             x, y = self.pos
@@ -63,7 +63,7 @@ class PersonaMalalta(RandomWalker):
     def step(self):
        
         self.durada_malaltia -= 1
-        if (self.durada_malaltia < 0):
+        if self.model.n_randoom() * 10 < self.model.mortalitat_virus or self.durada_malaltia < 0:
             x, y = self.pos
             this_cell = self.model.grid.get_cell_list_contents([self.pos])
             persona = [obj for obj in this_cell if isinstance(obj, PersonaMalalta)]
@@ -99,7 +99,7 @@ class PersonaImmunitzada(RandomWalker):
         self.random_move()
         living = True
 
-        if  self.random.random() * 10 < self.model.mutacio_virus:
+        if  self.model.n_random * 10 < self.model.mutacio_virus:
             x, y = self.pos
             this_cell = self.model.grid.get_cell_list_contents([self.pos])
             persona = [obj for obj in this_cell if isinstance(obj, PersonaImmunitzada)]
@@ -113,7 +113,7 @@ class PersonaImmunitzada(RandomWalker):
             self.model.schedule.add(persona_saludable)
             
 
-        if (self.random.random() * 100) < (self.model.reproduccio_persones):
+        if (self.model.n_random * 100) < (self.model.reproduccio_persones):
             virus = PersonaSaludable(
                 self.model.next_id(), self.pos, self.model, self.moore
             )       
